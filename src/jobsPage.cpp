2080dcbd708ca73700a131fdac6533428037838a
wdgbh5@umsystem.edu <wdgbh5@umsystem.edu>
2026-05-06 14:37:40 -0500

    Add support for editing cuttape data in jobs page

--- src/jobsPage.cpp
+++ src/jobsPage.cpp
@@ -2,31 +2,62 @@
 
 #include "board.hpp"
 
-jobsPage::jobsPage(QWidget* parent) : QWidget(parent)
+jobsPage::jobsPage(std::shared_ptr<Components> board_instance, QWidget* parent) : QWidget(parent)
 {
-    m_pJobsPageLayout = new QGridLayout();
-    this->setLayout(m_pJobsPageLayout);
+    m_pBoard_instance = board_instance;
 
-    // TODO: Add components to table through PnP->Components.getPlacementMap() and update table when new job is uploaded.
-    m_pJobsTable = new QTableWidget(10, 4);
+    m_pLayout         = new QGridLayout();
+    this->setLayout(m_pLayout);
+
+    m_pTable      = new QTableWidget(0, COUNT);
+
+    m_pEmptyLabel = new QLabel("No Job Uploaded", this);
+    m_pEmptyLabel->setStyleSheet("color: red;");
+    m_pEmptyLabel->setAlignment(Qt::AlignCenter);
+    QFont font = m_pEmptyLabel->font();
+    font.setPointSize(32);
+    font.setBold(true);
+    m_pEmptyLabel->setFont(font);
 
     QStringList horzHeaders;
-    horzHeaders << "Reference" << "Value" << "Cut Tape ID" << "Package";
-    m_pJobsTable->setHorizontalHeaderLabels(horzHeaders);
+    horzHeaders << "Qty" << "Value" << "Package" << "Pitch" << "Width" << "Orientation" << "X";
+    m_pTable->setHorizontalHeaderLabels(horzHeaders);
 
-    m_pUploadJobButton    = new QPushButton("Upload Job", this);
-    m_pCurrentJobTextEdit = new QTextEdit("", this);
+    m_pUploadJobButton        = new QPushButton("Upload Job", this);
+    m_pUpdateComponentsButton = new QPushButton("Update Components", this);
+    m_pCurrentJobTextEdit     = new QTextEdit("", this);
 
-    QFontMetrics fm       = QFontMetrics(m_pCurrentJobTextEdit->font());
-    int line_height       = fm.lineSpacing();
-    int margins           = m_pCurrentJobTextEdit->document()->documentMargin() + m_pCurrentJobTextEdit->frameWidth() * 2;
+    m_pPlacementMapSize       = new QLabel("Components to Place: ", this);
+    m_pLostMapSize            = new QLabel("Unknown Cuttapes: ", this);
+
+    QFontMetrics fm           = QFontMetrics(m_pCurrentJobTextEdit->font());
+    int line_height           = fm.lineSpacing();
+    int margins               = m_pCurrentJobTextEdit->document()->documentMargin() + m_pCurrentJobTextEdit->frameWidth() * 2;
     m_pCurrentJobTextEdit->setMaximumHeight(line_height + (int) margins * 2);
 
-    m_pJobsPageLayout->addWidget(m_pJobsTable, 0, 0, 1, 2);
-    m_pJobsPageLayout->addWidget(m_pCurrentJobTextEdit, 1, 0);
-    m_pJobsPageLayout->addWidget(m_pUploadJobButton, 1, 1);
+    QPixmap pix("../images/Cuttape.png");
+    m_pCuttapeImage = new QLabel(this);
+    m_pCuttapeImage->setPixmap(pix);
+    m_pCuttapeImage->setFixedSize(pix.width() / 2.5, pix.height() / 2.5);
+    m_pCuttapeImage->setScaledContents(true);
+
+    m_pLayout->addWidget(m_pTable, 0, 0, 3, 2);
+    m_pLayout->addWidget(m_pEmptyLabel, 0, 0, 3, 2);
+    m_pLayout->addWidget(m_pCurrentJobTextEdit, 3, 0);
+    m_pLayout->addWidget(m_pUploadJobButton, 3, 1);
+    m_pLayout->addWidget(m_pUpdateComponentsButton, 3, 3);
+    m_pLayout->addWidget(m_pCuttapeImage, 0, 3);
+    m_pLayout->addWidget(m_pPlacementMapSize, 1, 3);
+    m_pLayout->addWidget(m_pLostMapSize, 2, 3);
+
+    m_pTable->setColumnWidth(AMOUNT, 30);
+    m_pTable->setColumnWidth(PACKAGE, 325);
+    m_pTable->setColumnWidth(REMOVE, 20);
+
+    updatePage();
 
     connect(m_pUploadJobButton, &QPushButton::clicked, this, &jobsPage::onUploadJobButtonClicked);
+    connect(m_pUpdateComponentsButton, &QPushButton::clicked, this, &jobsPage::onUpdateComponentsButtonClicked);
 
     qDebug() << "jobsPage initialized.";
 }
@@ -68,35 +99,150 @@ void jobsPage::onUploadJobButtonClicked()
 
     qDebug() << "Job File Path: |" << m_sJobFilePath.c_str() << "|";
 
-    // TODO: Update to go through pnp class when components class is integrated
-    Components components(m_sJobFilePath.c_str());
+    m_pBoard_instance->parseCSV(m_sJobFilePath.c_str());
+    updatePage();
+}
 
-    std::map<std::tuple<std::string, cuttape_t>, std::vector<component_t>> component_map = components.getPlacementMap();
-    int i                                                                                = 0;
+void jobsPage::updatePage()
+{
+    m_pTable->clearContents();
+    Components::placement_map_t component_map = m_pBoard_instance->getPlacementMap();
+    Components::lost_cuttape_map_t lost_map   = m_pBoard_instance->getLostCuttapes();
 
-    qDebug() << "Size of map:" << component_map.size();
-    m_pJobsTable->setRowCount(component_map.size());
-
-    for (const auto& pair : component_map)
+    int placement_map_size                    = 0;
+    int row                                   = 0;
+    m_pTable->setRowCount(component_map.size() + lost_map.size());
+    for (Components::placement_map_t::iterator cuttape_it = component_map.begin(); cuttape_it != component_map.end(); cuttape_it++)
     {
-        // Safety check to ensure we don't access an empty component array leading to a SegFault (.end())
-        if (pair.second.empty())
-            continue;
+        QTableWidgetItem* number_of_components = new QTableWidgetItem(QString::fromStdString(std::to_string(cuttape_it->second.size())));
+        m_pTable->setItem(row, AMOUNT, number_of_components);
 
-        auto test3                  = pair.second.begin();
-        QTableWidgetItem* reference = new QTableWidgetItem(QString::fromStdString(test3->ref));
-        QTableWidgetItem* value     = new QTableWidgetItem(QString::fromStdString(test3->value));
-        QTableWidgetItem* ID        = new QTableWidgetItem(QString::fromStdString(std::to_string(std::get<1>(pair.first).ID)));
-        QTableWidgetItem* package   = new QTableWidgetItem(QString::fromStdString(test3->package));
+        QTableWidgetItem* value = new QTableWidgetItem(QString::fromStdString(std::get<0>(cuttape_it->first)));
+        m_pTable->setItem(row, VALUE, value);
 
-        qDebug() << i;
-        qDebug() << reference->text() << ", " << value->text() << ", " << ID->text() << ", " << package->text();
+        QTableWidgetItem* package = new QTableWidgetItem(QString::fromStdString(std::get<1>(cuttape_it->first)));
+        m_pTable->setItem(row, PACKAGE, package);
 
-        m_pJobsTable->setItem(i, 0, reference);
-        m_pJobsTable->setItem(i, 1, value);
-        m_pJobsTable->setItem(i, 2, ID);
-        m_pJobsTable->setItem(i, 3, package);
+        QLineEdit* pitchLine = new QLineEdit();
+        pitchLine->setText(QString::number(std::get<2>(cuttape_it->first).pitch, 'f', 3));
+        m_pTable->setCellWidget(row, PITCH, pitchLine);
 
-        i++;
+        QLineEdit* widthLine = new QLineEdit();
+        widthLine->setText(QString::number(std::get<2>(cuttape_it->first).width, 'f', 3));
+        m_pTable->setCellWidget(row, WIDTH, widthLine);
+
+        QStringList orientations;
+        orientations << "None" << "C1" << "C2" << "C3" << "C4" << "M1";
+        QComboBox* orientationCombo = new QComboBox();
+        orientationCombo->addItems(orientations);
+        orientationCombo->setCurrentIndex(std::get<2>(cuttape_it->first).orient);
+        m_pTable->setCellWidget(row, ORIENT, orientationCombo);
+
+        QTableWidgetItem* removeCheck = new QTableWidgetItem();
+        removeCheck->setFlags(Qt::ItemIsUserCheckable | Qt::ItemIsEnabled);
+        removeCheck->setCheckState(Qt::Unchecked);
+        m_pTable->setItem(row, REMOVE, removeCheck);
+
+        placement_map_size += cuttape_it->second.size();
+        row++;
     }
+
+    // Lost Cuttapes
+    for (Components::lost_cuttape_map_t::iterator cuttape_it = lost_map.begin(); cuttape_it != lost_map.end(); cuttape_it++)
+    {
+        QTableWidgetItem* number_of_components = new QTableWidgetItem(QString::fromStdString(std::to_string(cuttape_it->second.size())));
+        m_pTable->setItem(row, AMOUNT, number_of_components);
+
+        QTableWidgetItem* value = new QTableWidgetItem(QString::fromStdString(std::get<0>(cuttape_it->first)));
+        m_pTable->setItem(row, VALUE, value);
+
+        QTableWidgetItem* package = new QTableWidgetItem(QString::fromStdString(std::get<1>(cuttape_it->first)));
+        m_pTable->setItem(row, PACKAGE, package);
+
+        QLineEdit* pitchLine = new QLineEdit();
+        pitchLine->setPlaceholderText("Pitch (mm)");
+        pitchLine->setStyleSheet("background-color: yellow;");
+        m_pTable->setCellWidget(row, PITCH, pitchLine);
+
+        QLineEdit* widthLine = new QLineEdit();
+        widthLine->setPlaceholderText("Width (mm)");
+        widthLine->setStyleSheet("background-color: yellow;");
+        m_pTable->setCellWidget(row, WIDTH, widthLine);
+
+        QStringList orientations;
+        orientations << "None" << "C1" << "C2" << "C3" << "C4" << "M1";
+        QComboBox* orientationCombo = new QComboBox();
+        orientationCombo->addItems(orientations);
+        orientationCombo->setStyleSheet("background-color: yellow;");
+        m_pTable->setCellWidget(row, ORIENT, orientationCombo);
+
+        row++;
+    }
+
+    m_pPlacementMapSize->setText(QString::fromStdString("Components to Place: " + std::to_string(placement_map_size)));
+    m_pLostMapSize->setText(QString::fromStdString("Unknown Cuttapes: " + std::to_string((lost_map.size()))));
+
+    if (row == 0)
+    {
+        m_pEmptyLabel->setVisible(true);
+        m_pTable->setVisible(false);
+    }
+    else
+    {
+        m_pEmptyLabel->setVisible(false);
+        m_pTable->setVisible(true);
+    }
+}
+
+void jobsPage::onUpdateComponentsButtonClicked()
+{
+    Components::placement_map_t component_map = m_pBoard_instance->getPlacementMap();
+    Components::lost_cuttape_map_t lost_map   = m_pBoard_instance->getLostCuttapes();
+
+    int row                                   = 0;
+
+    for (Components::placement_map_t::iterator cuttape_it = component_map.begin(); cuttape_it != component_map.end(); cuttape_it++)
+    {
+        float user_pitch     = (qobject_cast<QLineEdit*>(m_pTable->cellWidget(row, PITCH))->text()).toFloat();
+        float user_width     = (qobject_cast<QLineEdit*>(m_pTable->cellWidget(row, WIDTH))->text()).toFloat();
+        int user_orientation = (qobject_cast<QComboBox*>(m_pTable->cellWidget(row, ORIENT)))->currentIndex();
+        bool isRemoved       = m_pTable->item(row, REMOVE)->checkState();
+
+        row++;
+        if (isRemoved)
+        {
+            m_pBoard_instance->removeFromPlacementMapToLostMap(get<Components::VAL>(cuttape_it->first),
+                                                               get<Components::PKG>(cuttape_it->first),
+                                                               get<Components::CTP>(cuttape_it->first));
+        }
+        else
+        {
+            if ((user_pitch != get<Components::CTP>(cuttape_it->first).pitch) || (user_width != get<Components::CTP>(cuttape_it->first).width) ||
+                (user_orientation != get<Components::CTP>(cuttape_it->first).orient))
+            {
+                m_pBoard_instance->updateCuttapeInPlacementMap(get<Components::VAL>(cuttape_it->first),
+                                                               get<Components::PKG>(cuttape_it->first),
+                                                               get<Components::CTP>(cuttape_it->first),
+                                                               {0, user_pitch, user_width, (orientation_t) user_orientation});
+            }
+        }
+    }
+
+    for (Components::lost_cuttape_map_t::iterator cuttape_it = lost_map.begin(); cuttape_it != lost_map.end(); cuttape_it++)
+    {
+        QString user_pitch      = qobject_cast<QLineEdit*>(m_pTable->cellWidget(row, PITCH))->text();
+        QString user_width      = qobject_cast<QLineEdit*>(m_pTable->cellWidget(row, WIDTH))->text();
+        QWidget* comboBoxWidget = m_pTable->cellWidget(row, ORIENT);
+        int user_orientation    = (qobject_cast<QComboBox*>(comboBoxWidget))->currentIndex();
+
+        row++;
+        if (!user_pitch.isEmpty() && !user_pitch.isEmpty())
+        {
+            m_pBoard_instance->addLostCuttapeToPlacementMap(get<Components::VAL>(cuttape_it->first),
+                                                            get<Components::PKG>(cuttape_it->first),
+                                                            {0, user_pitch.toFloat(), user_width.toFloat(), (orientation_t) user_orientation});
+        }
+    }
+
+    updatePage();
 }
